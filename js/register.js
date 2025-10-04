7b5bb2540287e23ad54afce2b23569e47fca2dc9
Shamia702 <shamiakhaliq09@gmail.com>
2025-10-04 19:55:59 +0200

    fixed my registration form functionality

--- js/register.js
+++ js/register.js
@@ -5,9 +5,10 @@ const registerForm = document.getElementById("registerForm");
 registerForm.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const name = document.getElementById("name").value.trim();
+  let name = document.getElementById("name").value.trim();
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
+
   name = name.replace(/\s+/g, "_").replace(/[^a-zA-Z0-9_]/g, "");
 
   try {
